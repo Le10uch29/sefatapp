4ee5f3472b72e3cc790b4fc79b80339e91d593ec
Elnur Mamedov <kal-el@list.ru>
2025-01-04 22:54:21 +0400

    initial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
-import { Provider } from 'react-redux';
-import ComponentDetail from "../Components/ContactDetail/ContactDetail";
+import { Provider } from "react-redux";
+import ClinicDetails from "../Components/ContactDetail/ContactDetail";
 import StatisticsCards from "../Components/StatisticsCards/StatisticsCards";
 import SearchBar from "../Components/SearchBar/SearchBar";
 import ExpenseStatistics from "../Components/ExpenseStatistics/ExpenseStatistics";
@@ -8,15 +8,15 @@ import LargeGraph from "../Components/LargeGraph/LargeGraph";
 import BalanceGraph from "../Components/BalanceGraph/BalanceGraph";
 import Application from "@/Components/Application/Application";
 import Header from "@/Components/Header/Header";
-import store from '../redux/store'
+import store from "../redux/store";
 import { UserProvider, useUserContext } from "@/Components/contex/UserContext";
-import Footer from '@/Components/Footer/Footer';
+import Footer from "@/Components/Footer/Footer";
 
 export default function App() {
   return (
     <Provider store={store}>
       <UserProvider>
-      <Home />
+        <Home />
       </UserProvider>
     </Provider>
   );
@@ -28,13 +28,13 @@ function Home() {
   return (
     <main className="main">
       <div className="container">
-      <Header />
+        <Header />
         <SearchBar />
         <h2 className="name-title">
           Salam {userData.Name} <span className="name-2th">Xoş gəldiniz!</span>
         </h2>
         <div className="section-1">
-          <ComponentDetail />
+          <ClinicDetails />
           <div className="charts">
             <ExpenseStatistics />
             <BalanceGraph />
